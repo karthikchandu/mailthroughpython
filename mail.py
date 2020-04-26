e4ea80c9dcdcc46518990938fd9062091759fe24
karthikchandu <57186207+karthikchandu@users.noreply.github.com>
2020-04-26 09:27:32 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -7,6 +7,6 @@ Created on Sun Apr 26 09:11:10 2020
 import smtplib
 server = smtplib.SMTP_SSL("smtp.gmail.com",465)
 server.login("examplefrom@gmail.com", "karthiktc@123")
-server.sendmail("examplefrom1999@gmail.com", "exampleTo@gmail.com","I love you")
+server.sendmail("examplefrom@gmail.com", "exampleTo@gmail.com","I love you")
 server.quit()
 
